1fa28ad3c0a102f1723ae02e2c8caa6128b60a27
DialloMamadouDjan <iamdiallo02@gmail.com>
2021-10-13 20:09:01 +0200

    cleaner prog

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
-import os
 import requests
-from flask import Flask, render_template, request, jsonify
-import json
+from flask import Flask, render_template, jsonify
 import datetime
-from random import *
-
 
 
 app = Flask(__name__)
@@ -29,7 +25,6 @@ def citySearch(city):
         for key in keys_to_remove:
             for i in range(len(cityWeather)):
                 cityWeather[i]['the_temp'] =int(cityWeather[i]['the_temp'])
-                cityWeather[i]['the_temp'] =str(cityWeather[i]['the_temp'])
                 del cityWeather[i][key]
 
                 if (cityWeather[i]['weather_state_name'] == "Showers" or
@@ -38,37 +33,33 @@ def citySearch(city):
                 cityWeather[i]['weather_state_name'] == "Heavy Cloud") :
 
                     link = "https://random.dog/woof.json"
-                    # link = requests.get(api).json()
-                    # link = link["url"]
+                    # link = requests.get(link).json()
+                    # link = link["url"]                    REQUEST IS LOW WHEN UNCOMMENTED
 
                 elif (cityWeather[i]['weather_state_name'] == "Sunny" or
                 cityWeather[i]['weather_state_name'] ==  "Light Cloud" or 
                 cityWeather[i]['weather_state_name'] == "Clear"
                 ):
                     link = "https://aws.random.cat/meow"
-                    # link = requests.get(link)).json()
+                    # link = requests.get(link).json()        REQUEST IS LOW WHEN UNCOMMENTED
                     # link = link["file"]
 
                 else :
                     link = "https://shibe.online/"
-
                 cityWeather[i]["picture_weather"] = link
-                cityWeather[i]['picture_weather'] = link
-        for i in range(len(cityWeather)) :
-            cityWeather[i]['applicable_date'] 
+        for i in range(len(cityWeather)) : 
             _date = cityWeather[i]['applicable_date']
-            _date = datetime.datetime.strptime(_date, '%Y-%m-%d')
-            _date= ('{0}-{1}-{2:02}'.format(_date.month, _date.day, _date.year))
-            _date= cityWeather[i]['weather_state_name']
-            _date= cityWeather[i]['the_temp']
-
+            _date = datetime.datetime.strptime(_date , '%Y-%m-%d')
+            _date  = ('{0}-{1}-{2:02}'.format(_date .month, _date .day, _date.year))
+            _Weather = cityWeather[i]['weather_state_name']
+            _Temp = cityWeather[i]['the_temp']
 
             del cityWeather[i]['applicable_date']
             del cityWeather[i]['weather_state_name']
             del cityWeather[i]['the_temp']
-            cityWeather[i]['Day'] = _date
-            cityWeather[i]['weather'] = _date
-            cityWeather[i]['temperature'] = _date
+            cityWeather[i]['Day'] = _date 
+            cityWeather[i]['weather'] = _Weather
+            cityWeather[i]['temperature'] = _Temp
 
         response["content"] = cityWeather
         return jsonify(response)
